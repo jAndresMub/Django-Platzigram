49508d9311e091a537826b0013226017c4ef00a2
andresm <jandresmub@gmail.com>
2020-06-04 17:57:09 -0500

    aplicacion con 3 fotos montadas, ya se tiene bootstrap incluido

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
 
 # Django
+
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 #Utilities
 from datetime import datetime
@@ -12,21 +13,21 @@ posts =[
         'name': 'Mont Blank',
         'user': 'Ninaia',
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/id/1000/200/300',
+        'picture': 'https://picsum.photos/id/100/200/200',
     },
 
     {
         'name': 'Some Other',
         'user': 'Denden',
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/id/1015/200/300',
+        'picture': 'https://picsum.photos/id/1015/200/200',
     },
 
     {
         'name': 'Other More',
         'user': 'Ninaia',
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/id/1019/200/300',
+        'picture': 'https://picsum.photos/id/1011/200/200',
     }
 
 
@@ -36,10 +37,4 @@ posts =[
 
 def list_post(request):
     content = []
-    for post in posts:
-        content.append("""
-        <p><strong>{name}</strong></p>
-        <p><small>{user} - <i>{timestamp}</i></small></p>
-        <figure><img src="{picture}"/></figure>
-        """.format(**post))
-    return HttpResponse('<br>'.join(content))
+    return render(request, 'feed.html', {'posts': posts})
